2dd164a44a7e801efb195f34455acf800478e16a
Alexandre Ferreira <alexandre8094@gmail.com>
2026-02-06 18:57:07 +0000

    Page ready for Cloudfare

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+export default {
+  content: [
+    "./index.html",
+    "./JS/**/*.js",
+    "./Controllers/**/*.js",
+    "./views/**/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
